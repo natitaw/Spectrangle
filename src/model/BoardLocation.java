7648162559de72605c465609ba686ecd7210a85c
Nate <nathnaelshenkute@gmail.com>
2019-01-31 20:40:16 +0100

    Documented BoardLocation.java

--- src/model/BoardLocation.java
+++ src/model/BoardLocation.java
@@ -7,68 +7,75 @@ package model;
  */
 class BoardLocation {
 
+	private final int location;
+	private final int scorePoint;
+	private Piece piece;
 
-    private final int location;
-    private final int scorePoint;
-    private Piece piece;
+	/**
+	 * Initialize Board location
+	 *
+	 * @param location   Number representing a location on the board
+	 * @param scorePoint The amounts of points attributed to a location (multiplier)
+	 */
+	public BoardLocation(int location, int scorePoint) {
+		this.location = location;
+		this.scorePoint = scorePoint;
 
-    /**
-     * Initialize Board location
-     *
-     * @param location
-     * @param scorePoint
-     */
-    public BoardLocation(int location, int scorePoint) {
-        this.location = location;
-        this.scorePoint = scorePoint;
+	}
 
-    }
 
-    // Queries
 
-    /**
-     * Return the amounts of point of this location
-     *
-     * @return
-     */
-    public int getScorePoint() {
-        return scorePoint;
-    }
+	/**
+	 * Return the amounts of point of this location
+	 *
+	 * @return the amounts of point of this location
+	 */
+	public int getScorePoint() {
+		return scorePoint;
+	}
 
-    /**
-     * Return the piece of this spot
-     *
-     * @return
-     */
-    public Piece getPiece() {
-        return this.piece;
-    }
+	/**
+	 * Return the piece of this spot
+	 *
+	 * @return Piece object
+	 */
+	public Piece getPiece() {
+		return this.piece;
+	}
 
-    /**
-     * Set a piece on this spot
-     *
-     * @param p
-     */
-    public void movePiece(Piece p) {
-        this.piece = p;
-    }
+	/**
+	 * Set a piece on this spot
+	 *
+	 * @param moves a Piece to this Object
+	 */
+	public void movePiece(Piece p) {
+		this.piece = p;
+	}
 
-    /**
-     * Check if this spot is a bonus location
-     *
-     * @return
-     */
-    public boolean isBonusLocation() {
-        return this.scorePoint > 1;
-    }
+	/**
+	 * Check if this spot is a bonus location
+	 *
+	 * @return Boolean verifying if this location has a bonus point or not
+	 */
+	public boolean isBonusLocation() {
+		return this.scorePoint > 1;
+	}
 
-    /**
-     * Check if this spot is an empty spot
-     *
-     * @return
-     */
-    public boolean isEmptySpot() {
-        return (this.piece == null);
-    }
+	/**
+	 * Check if this spot is an empty spot
+	 *
+	 * @return Boolean verifying the emptiness of the location
+	 */
+	public boolean isEmptySpot() {
+		return (this.piece == null);
+	}
+	
+	/**
+	 * A method that returns the location field of this object
+	 * @return Location value of the object
+	 */
+	public int getLocation() {
+		return this.location;
+	}
 
 }
